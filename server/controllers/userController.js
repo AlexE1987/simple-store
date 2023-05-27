197d2df5b3f2b44212cc113f3f3e6fc91e510282
Aleksey Evsikov <evsikovad1987@gmail.com>
2023-05-27 16:23:52 +0400

    update routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,14 @@
-const { json } = require('sequelize');
+const ApiError = require('../error/apiError');
 
 class UserController {
   async registration(req, res) {}
   async login(req, res) {}
-  async check(req, res) {
-    res.json('sssssssss');
+  async check(req, res, next) {
+    const { id } = req.query;
+    if (!id) {
+      return next(ApiError.badRequest('Не задан ID'));
+    }
+    res.json(id);
   }
 }
 
