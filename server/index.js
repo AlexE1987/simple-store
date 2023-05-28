54d971d8627533d94634bfca8451ddb58f0d0e1c
Aleksey Evsikov <evsikovad1987@gmail.com>
2023-05-28 14:49:24 +0400

    implement: auth

--- server/index.js
+++ server/index.js
@@ -4,9 +4,8 @@ const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
-
 const router = require('./routes/index');
-const errorHandler = require('./middlware/errorHandlingMiddlware');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 const path = require('path');
 
 const PORT = process.env.PORT || 5000;
@@ -18,14 +17,14 @@ app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
 app.use('/api', router);
 
+// Обработка ошибок, последний Middleware
 app.use(errorHandler);
 
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-
-    app.listen(PORT, () => console.log(`Server listen on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (e) {
     console.log(e);
   }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,13 @@
-const Router = require('express');
-const router = new Router();
+const Router = require('express')
+const router = new Router()
+const deviceRouter = require('./deviceRouter')
+const userRouter = require('./userRouter')
+const brandRouter = require('./brandRouter')
+const typeRouter = require('./typeRouter')
 
-const brandRouter = require('./brandRouter');
-const deviceRouter = require('./deviceRouter');
-const typeRouter = require('./typeRouter');
-const userRouter = require('./userRouter');
+router.use('/user', userRouter)
+router.use('/type', typeRouter)
+router.use('/brand', brandRouter)
+router.use('/device', deviceRouter)
 
-router.use('/brand', brandRouter);
-router.use('/device', deviceRouter);
-router.use('/type', typeRouter);
-router.use('/user', userRouter);
-
-module.exports = router;
+module.exports = router
